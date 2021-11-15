13334b5b9655a6e543e3b595133a96c49d3ac6b2
Alex Snell <alexksnell@gmail.com>
2021-11-14 22:07:58 -0500

    [ADD]: Implement User model skeleton of UserDAO

---
+++ src/main/java/com/revature/app/models/User.java
@@ -0,0 +1,66 @@
+package main.java.com.revature.app.models;
+
+public class User {
+
+    private String id;
+    private String username;
+    private String passwordHash;
+    private String passwordSalt;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the passwordSalt
+     */
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    /**
+     * @param passwordSalt the passwordSalt to set
+     */
+    public void setPasswordSalt(String passwordSalt) {
+        this.passwordSalt = passwordSalt;
+    }
+
+    /**
+     * @return the passwordHash
+     */
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    /**
+     * @param passwordHash the passwordHash to set
+     */
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
